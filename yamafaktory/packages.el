5e4b477cbc1ab8ce2c040da8eed724e2422cff58
davy <davy@qipp.com>
2015-09-22 12:18:09 +0200

    Updated multi-cursors bindings.

--- yamafaktory/packages.el
+++ yamafaktory/packages.el
@@ -102,9 +102,11 @@
 
 ;; multiple-cursors
 (require-pkg 'multiple-cursors t)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
 
 ;; multi-term
 (require-pkg 'multi-term t)
