dc9b0bbbec3239b48ff5d2bfbbb3c2c5bb16c270
yamafaktory <davy@qipp.com>
2016-03-11 11:22:00 +0100

    Updated utilities.

--- yamafaktory/utilities.el
+++ yamafaktory/utilities.el
@@ -5,12 +5,12 @@
 
 ;;; Code:
 
-(defun change-js-indent (spaces)
+(defun js-indent (spaces)
   "Change the javascript indentation."
-  (interactive "p")
+  (interactive "nPlease enter the new indentation size in spaces:")
   (setq-default js2-basic-offset spaces))
 
-(defun kill-other-buffers ()
+(defun clean-buffers ()
   "Kill all buffers except current and special buffers."
   (interactive)
   (dolist (buffer (buffer-list))
