13dccc6fed8b153fc57c4dfc05642d54df189e90
yamafaktory <davy@qipp.com>
2016-05-11 09:24:22 +0200

    Updated web-mode settings.

--- yamafaktory/packages.el
+++ yamafaktory/packages.el
@@ -188,18 +188,25 @@
 
 ;; web-mode
 (require-pkg 'web-mode t)
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (defun custom-web-mode-hook ()
   "Custom hooks for web-mode."
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-auto-pairing nil)
   (setq web-mode-enable-css-colorization t)
   (setq web-mode-enable-current-element-highlight t))
+(defun smartparens-in-web-mode (id action context)
+  (and (eq action 'insert)
+       (not (or (get-text-property (point) 'part-side)
+                (get-text-property (point) 'block-side)))))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))
 (add-hook 'web-mode-hook 'custom-web-mode-hook)
+(sp-local-pair 'web-mode "<" nil :when '(smartparens-in-web-mode))
 
 ;; windmove
 (require-pkg 'windmove t)
