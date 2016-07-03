51ab7251b9cfb004d9f2a101808a52b175027240
yamafaktory <yamafaktory@gmail.com>
2016-07-03 22:37:42 +0200

    Fixed switch offset with last js2-mode.

--- yamafaktory/packages.el
+++ yamafaktory/packages.el
@@ -98,7 +98,7 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'"  . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
 (setq-default js2-basic-offset 2
-              js2-indent-switch-body t
+              js-switch-indent-offset 2
               js2-mode-show-parse-errors nil
               js2-mode-show-strict-warnings nil)
 
