3ac9533cb0c7fdb641ea5fedfbfa439fd778fe2c
yamafaktory <yamafaktory@gmail.com>
2016-12-27 08:10:50 +0100

    Updated markdown mode.

--- yamafaktory/packages.el
+++ yamafaktory/packages.el
@@ -133,6 +133,7 @@
 (add-to-list 'auto-mode-alist '("\\.text\\'"     . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'"       . markdown-mode))
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
 ;; midnight
 (require-pkg 'midnight t)
